f6013fd9a843ab244c9aa0a180ce4661ce5cb60d
Matheus Dutra <matheus.yusuke.12@gmail.com>
2023-07-18 09:58:29 -0300

    Alteração para mostrar somente um produto de cada categoria ao carregar a página, e todos os produtos ao filtrar por categoria

--- assets/js/filtrarCategoria.js
+++ assets/js/filtrarCategoria.js
@@ -7,7 +7,6 @@ cardsCategoria.forEach(card => {
         const categoria = card.dataset.categoria
         const categoriaUrl = `http://localhost:3000/${categoria}`
         let repeticao = "tamanhoProduto"
-        const teste = console.log(categoriaUrl.length)
 
         cardsTitulo.innerHTML = `Categoria: ${card.children[1].textContent} <span class="escondeVisualmente"> Lista de produtos.</span>`
         cardsProduto.innerHTML = ""
